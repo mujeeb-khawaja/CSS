9d497fc92dc9c4aaad46543210adc22a52cfd543
codi-man <mujeebkhawaja29@gmail.com>
2025-11-05 11:40:44 +0500

    Made a faulty calculator

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const func1 = () => "I am func 1";
+// faulty calculator
 
-function result(a, b, c = 7) {
-  console.log(func1())            // runs (side effect)
-  return a + b + c;
+
+function randomInt() {
+  return Math.floor(Math.random() * 101);
 }
 
-console.log(result(2, 3)); // 12
+function faultyCalculator(num1, num2, operator) {
+  if (operator === '+') {
+    return randomInt() < 10 ? num1 - num2 : num1 + num2;
+  }
+  else if (operator === '-') {
+    return num1 - num2;
+    } else if (operator === '*') {
+    return num1 * num2;
+    } else if (operator === '/') {
+    return num1 / num2;
+    } else {
+    return "Invalid operator";
+    }
+}
+
+const num1 = 50;
+const num2 = 10;
+const operator = '+'; // Try '+', '-', '*', or '/'
+
+console.log(`The result of ${num1} ${operator} ${num2} is: ${faultyCalculator(num1, num2, operator)}`);
+console.log(`Number returned from randomInt(): ${randomInt()}`);
