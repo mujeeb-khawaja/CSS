ea69118e2bdcc470a67100ad0ca8e885116d49dd
codi-man <mujeebkhawaja29@gmail.com>
2025-11-11 13:56:37 +0500

    Video 63

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-let a = "Harry"
-console.log(a.indexOf("ar"))
+let arr = [1,2,3,4,5];
+let new_arr = arr.map(e => {
+  return e * 2;
+})
+
+const greaterThanTwo = (e) => {
+  if(e>2) {
+    return true;
+  }
+  return false;
+}
+
+console.log(arr.filter(greaterThanTwo));
