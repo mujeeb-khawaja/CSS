509bfd4e1b13236b182de73b9ab7d72a332f8db7
codi-man <mujeebkhawaja29@gmail.com>
2025-11-18 16:05:01 +0500

    Error handling

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
+function randomNumber() {
+    return Math.floor(Math.random() * 7);
+}
+
 async function getData() {
-    
-    let x = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            title: 'Code',
-            body: 'Mujeeb',
-            userId: 1,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-    let data = await x.json();
-    return data;
+    let randomNumberValue = randomNumber();
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Initializing Hacking...");
+        }, randomNumberValue*1000);
+    });
 }
 
 async function main() {
-    console.log("Hello, World!");
-    console.log("Load data");
     let data = await getData();
     console.log(data);
-
-    console.log("Process data");
 }
-
 main()
