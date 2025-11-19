1453162902fb214895f53eb71de3127bce827e9b
codi-man <mujeebkhawaja29@gmail.com>
2025-11-19 12:47:52 +0500

    video 80

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-let a = prompt("Enter first number:");
-let b = prompt("Enter second number:");
-
-if(Number.isNaN(a) || Number.isNaN(b)) {
-    throw SyntaxError("Input values must be numbers");
+class Animal {
+    constructor(name) {
+        this.name = name;
+        console.log("Constructor created")
+    }
+    eats() {
+        console.log(`${this.name} eats food.`);
+    }
+    jumps() {
+        console.log(`${this.name} jumps high.`);
+    }
 }
+
+class Rabbit extends Animal {
+    constructor(name) {
+        super(name);
+        console.log("Rabbit constructor created")
+    }
+}
+
+let a = new Animal("Dog");
+let x = new Rabbit("Bunny");
